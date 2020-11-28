bc0e598dbe27e0c61cbeeaa4f9a998fa7d5c0597
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-11-28 12:59:16 +0100

    Model item entity updates

---
+++ sources/IoT.Simulator/IoT.Simulator/Models/DTDLModelItem.cs
@@ -0,0 +1,8 @@
+﻿namespace IoT.Simulator.Models
+{
+    public class DTDLModelItem
+    {
+        public string ModelId { get; set; }
+        public DTDLModelType ModelType { get; set; }
+    }
+}
