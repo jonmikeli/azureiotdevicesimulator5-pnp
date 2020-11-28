7de9e4e682875f58e4471c3c69e858ef12f5c291
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-11-28 12:55:49 +0100

    DTDLHelper

---
+++ sources/IoT.Simulator/IoT.Simulator/Tools/DTDLHelper.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoT.Simulator.Tools
+{
+    public class DTDLHelper
+    {
+        public static JObject BuildMessageBodyFromDTDL(JObject dtdl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static JObject GetDTDLFromModelId(string modelId, string modelRepositoryPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
