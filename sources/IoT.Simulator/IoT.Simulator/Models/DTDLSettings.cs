28c5bc968117078814aacd2ed116f07d4e1d006c
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-11-28 13:14:32 +0100

    DTDL Model settings

---
+++ sources/IoT.Simulator/IoT.Simulator/Models/DTDLSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace IoT.Simulator.Models
+{
+    public class DTDLSettings
+    {
+        public DTDLModelItem DefaultModel { get; set; }
+
+        public IEnumerable<DTDLModelItem> Models { get; set; }
+    }
+}
