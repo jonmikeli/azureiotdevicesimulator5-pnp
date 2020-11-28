e424830c51f4abe041fcb57d3268c1366ff1bc83
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-11-28 19:54:27 +0100

    part of the building messages logic

--- sources/IoT.Simulator/IoT.Simulator/Tools/DTDLHelper.cs
+++ sources/IoT.Simulator/IoT.Simulator/Tools/DTDLHelper.cs
@@ -8,12 +8,33 @@ using System.Threading.Tasks;
 
 namespace IoT.Simulator.Tools
 {
+    //https://docs.microsoft.com/en-us/azure/iot-pnp/concepts-model-parser
     public class DTDLHelper
     {
 
-        public static JObject BuildMessageBodyFromModelId(string modelId)
+        public static JObject BuildMessageBodyFromModelId(string modelId, string[] locations)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(modelId))
+                throw new ArgumentNullException(nameof(modelId));
+
+            if (locations == null | !locations.Any())
+                throw new ArgumentNullException(nameof(locations));
+
+            //Get the full DTDL model
+            JObject dtdlModel = null;
+            int i = 0;
+            while (dtdlModel == null && i < locations.Length)
+            {
+                dtdlModel = GetDTDLFromModelId(modelId, locations[i]);
+                i++;
+            }
+
+            if (dtdlModel == null)
+                throw new Exception($"No DTDL model with the id {modelId} has been provided at the provided location {}.");
+
+
+            //Build the JSON Message corresponding to the model
+            return BuildMessageBodyFromDTDL(dtdlModel);
         }
 
         public static JObject BuildMessageBodyFromDTDL(JObject dtdl)
@@ -23,6 +44,7 @@ namespace IoT.Simulator.Tools
 
         public static JObject GetDTDLFromModelId(string modelId, string modelRepositoryPath)
         {
+            //TODO add a cache system to optimize the calls
             throw new NotImplementedException();
         }
     }
