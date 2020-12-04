baae8360b9c5902a8771dc848d700856ff001243
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-12-04 23:54:43 +0100

    DTDL command handler context

---
+++ sources/IoT.Simulator/IoT.Simulator/Services/IoT/DTDLCommandHandlerContext.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoT.Simulator.Services.IoT
+{
+    public class DTDLCommandHandlerContext
+    {
+        public string CommandName { get; set;}
+        public JToken CommandRequestPayload { get; set; }
+        public JToken CommandResponsePayload { get; set; }
+    }
+}
