9d417223d0aad2681809921f18484d809665adbf
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2021-01-26 21:45:44 +0100

    references cleaning

--- sources/IoT.Simulator/IoT.DTDL.Tests/DTDLTests.cs
+++ sources/IoT.Simulator/IoT.DTDL.Tests/DTDLTests.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using Newtonsoft.Json.Linq;
-
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace IoT.DTDL.Tests
