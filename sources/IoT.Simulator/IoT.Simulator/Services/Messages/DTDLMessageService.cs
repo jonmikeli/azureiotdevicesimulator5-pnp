bb39243c52ccdbda0e26c4b3b2ab9ff79a24f3a2
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2021-02-03 23:15:14 +0100

    telemtry message creation updates

--- sources/IoT.Simulator/IoT.Simulator/Services/Messages/DTDLMessageService.cs
+++ sources/IoT.Simulator/IoT.Simulator/Services/Messages/DTDLMessageService.cs
@@ -18,7 +18,7 @@ namespace IoT.Simulator.Services
         private ILogger _logger;
 
         public DTDLMessageService(ILoggerFactory loggerFactory)
-        {            
+        {
             if (loggerFactory == null)
                 throw new ArgumentNullException(nameof(loggerFactory));
 
@@ -27,22 +27,29 @@ namespace IoT.Simulator.Services
 
         public async Task<string> GetMessageAsync(string modelId, string modelPath)
         {
+            string messageString = string.Empty;
 
             var modelContainer = await DTDLHelper.GetModelsAndBuildDynamicContentAsync(modelId, modelPath);
 
             if (modelContainer == null)
                 throw new Exception($"No model container has been found corresponding to the parameters provided:: modelId: {modelId} - modelPath: {modelPath}");
 
-            var modelContent = modelContainer.SingleOrDefault(i => i.Key == modelId);
-            if (modelContent.Equals(default(KeyValuePair<string, DTDLContainer>)))
-                throw new Exception($"No model corresponding to the modelId {modelId} has been found.");
+            //var modelContent = modelContainer.SingleOrDefault(i => i.Key == modelId);
+            var contentWithTelemetries = modelContainer.Where(i => i.Value != null && i.Value.DTDLGeneratedData != null && i.Value.DTDLGeneratedData.Telemetries != null);
 
-            string messageString = string.Empty;
-            if (
-                modelContent.Value != null
-                && modelContent.Value.DTDLGeneratedData != null
-                && modelContent.Value.DTDLGeneratedData.Telemetries != null)
-                messageString = JsonConvert.SerializeObject(modelContent.Value.DTDLGeneratedData.Telemetries, Formatting.Indented);
+            if (contentWithTelemetries != null && contentWithTelemetries.Any())
+            {
+                JArray telemetries = new JArray();
+                foreach (var item in contentWithTelemetries)
+                {
+                    foreach (var telemetry in item.Value.DTDLGeneratedData.Telemetries)
+                    {
+                        telemetries.Add(telemetry);
+                    }
+                }
+
+                messageString = JsonConvert.SerializeObject(telemetries, Formatting.Indented);
+            }
             else
                 throw new ArgumentException($"No telemetry has been built from the provided model::modelId: {modelId} - modelPath: {modelPath}");
 
