fe1d8ac7b24c7d04b22dd85c6e3c947e5efb2ff6
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2021-08-29 23:58:55 +0200

    unit tests updates

--- sources/IoT.Simulator/IoT.DTDL/DTDLHelper.cs
+++ sources/IoT.Simulator/IoT.DTDL/DTDLHelper.cs
@@ -407,6 +407,23 @@ namespace IoT.DTDL
                 result.DTDLGeneratedData.ReadableProperties = readableProperties;
             }
 
+            readableProperties = ExtractReadablePropertiesWithUnit(contents);
+            if (readableProperties != null && readableProperties.Any())
+            {
+                if (result.DTDLGeneratedData == null)
+                    result.DTDLGeneratedData = new DTDLGeneratedData();
+
+                if (result.DTDLGeneratedData.ReadableProperties == null)
+                    result.DTDLGeneratedData.ReadableProperties = readableProperties;
+                else
+                {
+                    foreach (var item in readableProperties)
+                    {
+                        result.DTDLGeneratedData.ReadableProperties.Add(item);
+                    }
+                }
+            }
+
             JArray writableProperties = ExtractWritableProperties(contents);
             if (writableProperties != null && writableProperties.Any())
             {
@@ -414,6 +431,23 @@ namespace IoT.DTDL
                     result.DTDLGeneratedData = new DTDLGeneratedData();
 
                 result.DTDLGeneratedData.WritableProperties = writableProperties;
+            }
+
+            writableProperties = ExtractWritablePropertiesWithUnit(contents);
+            if (writableProperties != null && writableProperties.Any())
+            {
+                if (result.DTDLGeneratedData == null)
+                    result.DTDLGeneratedData = new DTDLGeneratedData();
+
+                if (result.DTDLGeneratedData.WritableProperties == null)
+                    result.DTDLGeneratedData.WritableProperties = writableProperties;
+                else
+                {
+                    foreach (var item in writableProperties)
+                    {
+                        result.DTDLGeneratedData.WritableProperties.Add(item);
+                    }
+                }
             }
 
             //Commands
@@ -496,7 +530,51 @@ namespace IoT.DTDL
                i =>
                    (i is JObject)
                    &&
+                   (i["@type"] is not JArray)
+                   &&
                    i["@type"].Value<string>().ToLower() == "property"
+                   &&
+                   ((JObject)i).ContainsKey("writable") && i["writable"].Value<bool>()
+               );
+
+
+            if (properties != null && properties.Any())
+            {
+                result = new JArray();
+
+                JObject tmp = null;
+                string tmpPropertyName = string.Empty;
+
+                Random random = new Random(DateTime.Now.Millisecond);
+                foreach (var item in properties)
+                {
+                    tmpPropertyName = item["name"].Value<string>();
+
+                    tmp = new JObject();
+
+                    JProperty jProperty = AddCreatedProperties(tmpPropertyName, item["schema"].Value<string>(), random);
+
+                    if (jProperty != null)
+                        tmp.Add(jProperty);
+
+                    result.Add(tmp);
+                }
+            }
+
+            return result;
+        }
+
+        private static JArray ExtractWritablePropertiesWithUnit(JArray contents)
+        {
+            JArray result = null;
+
+            var properties = contents.Where(
+               i =>
+                   (i is JObject)
+                   &&
+                   (i["@type"] is JArray)
+                   &&
+                   ((JArray)i["@type"])[0].Value<string>().ToLower() == "property"
                    &&
                    ((JObject)i).ContainsKey("writable") && i["writable"].Value<bool>()
                );
@@ -535,7 +613,52 @@ namespace IoT.DTDL
                 i =>
                     (i is JObject)
                     &&
+                    i["@type"] is not JArray
+                    &&
                     i["@type"].Value<string>().ToLower() == "property"
+                    &&
+                    (
+                        !((JObject)i).ContainsKey("writable")
+                        |
+                        (((JObject)i).ContainsKey("writable") && !i["writable"].Value<bool>())
+                    )
+                );
+
+            if (properties != null && properties.Any())
+            {
+                result = new JArray();
+                JObject tmp = null;
+                string tmpPropertyName = string.Empty;
+
+                Random random = new Random(DateTime.Now.Millisecond);
+                foreach (var item in properties)
+                {
+                    tmpPropertyName = item["name"].Value<string>();
+
+                    tmp = new JObject();
+
+                    JProperty jProperty = AddCreatedProperties(tmpPropertyName, item["schema"].Value<string>(), random);
+
+                    if (jProperty != null)
+                        tmp.Add(jProperty);
+
+                    result.Add(tmp);
+                }
+            }
+
+            return result;
+        }
+
+        private static JArray ExtractReadablePropertiesWithUnit(JArray contents)
+        {
+            JArray result = null;
+            var properties = contents.Where(
+                i =>
+                    (i is JObject)
+                    &&
+                    i["@type"] is JArray
+                    &&
+                    ((JArray)i["@type"])[0].Value<string>().ToLower() == "property"
                     &&
                     (
                         !((JObject)i).ContainsKey("writable")
@@ -572,7 +695,7 @@ namespace IoT.DTDL
         private static JArray ExtractCommands(JArray contents)
         {
             JArray result = null;
-            var commands = contents.Where(i => i["@type"].Value<string>().ToLower() == "command");
+            var commands = contents.Where(i => i["@type"] is not JArray && i["@type"].Value<string>().ToLower() == "command");
             if (commands != null && commands.Any())
             {
                 result = new JArray();
